fd7b224f55864a0d6e82018b8bd280e06ea1d866
TownTowner <tanghelv@qq.com>
2025-06-30 17:55:06 +0800

    feat: 添加基于OpenCV的新版多目标跟踪实现
    新增SimpleMultiTracker类实现多目标跟踪功能，支持多种跟踪算法选择
    根据OpenCV版本自动选择使用新接口或旧接口
    添加类型提示和示例视频文件路径

--- opencv/imgObjectTracker.py
+++ opencv/imgObjectTracker.py
@@ -12,6 +12,7 @@
 # GOTURN Tracker：这是OpenCV中唯一一深度学习为基础的目标检测器。它需要额外的模型才能运行。（最低
 # 支持OpenCV3.2.0）
 
+from typing import List
 import cv2
 import numpy as np
 
@@ -73,8 +74,78 @@ def legacy():
     cv2.destroyAllWindows()
 
 
+def current():
+    print(f"{(cv2.__version__)}")
+
+    # print([attr for attr in dir(cv2) if "Tracker" in attr])
+    class SimpleMultiTracker:
+        trackers: List[cv2.Tracker] = []
+
+        def __init__(self):
+            pass
+
+        def add(self, tracker, frame, bbox):
+            tracker.init(frame, bbox)
+            self.trackers.append(tracker)
+
+        def update(self, frame):
+            boxes = []
+            for tracker in self.trackers:
+                success, box = tracker.update(frame)
+                if success:
+                    boxes.append(box)
+            return True, boxes
+
+    # 使用示例
+    trackers = SimpleMultiTracker()
+    tracker_dict = {
+        # "boosting": cv2.TrackerBoosting,
+        "mil": cv2.TrackerMIL().create,
+        "kcf": cv2.TrackerKCF().create,
+        "csrt": cv2.TrackerCSRT().create,
+        # "medianflow": cv2.TrackerMedianFlow,
+        # "tld": cv2.TrackerTLD,
+        # "mosse": cv2.TrackerMOSSE,
+        "nano": cv2.TrackerNano().create,
+        "vit": cv2.TrackerVit().create,
+    }
+
+    # cap = cv2.VideoCapture("data/videos/soccer.mp4")
+    cap = cv2.VideoCapture("data/videos/racecar.mp4")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("无法读取视频帧")
+            break
+
+        success, boxes = trackers.update(frame)
+        for box in boxes:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("frame", frame)
+
+        k = cv2.waitKey(100) & 0xFF
+        if k == ord("s"):
+            roi = cv2.selectROI("frame", frame)
+            # cv2.TrackerCSRT().init(frame,roi)
+            tracker = tracker_dict["csrt"]()
+            trackers.add(tracker, frame, roi)
+        elif k == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def main():
-    legacy()
+    v = cv2.__version__.split(".")
+    v = float(f"{v[0]}.{v[1]}")
+
+    if v > 4.5:
+        current()
+    elif v <= 4.5:
+        legacy()
 
 
 if __name__ == "__main__":
